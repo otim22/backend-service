aabb04aa924ba36a17e9fd2329dde70415173b55
Otim Fredrick <fredrickot@gmail.com>
2023-05-09 16:03:19 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+import express, { Application, Request, Response } from 'express';
+import * as http from 'http';
+import dotenv from 'dotenv';
+
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import cors from 'cors';
+import { CommonRoutesConfig } from './common/routes.config';
+import { UsersRoutes } from './users/routes.config';
+import debug, { IDebugger } from 'debug';
+
+dotenv.config();
+const app: Application = express();
+const server: http.Server = http.createServer(app);
+const routes: Array<CommonRoutesConfig> = [];
+const debugLog: IDebugger = debug('app');
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+const loggerOptions: expressWinston.LoggerOptions = {
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize({ all: true })
+    ),
+};
+
+if (!process.env.DEBUG) {
+    loggerOptions.meta = false;
+}
+
+app.use(expressWinston.logger(loggerOptions));
+routes.push(new UsersRoutes(app));
+
+const runningMessage = `Server running at http://localhost:${port}`;
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send(runningMessage)
+});
+
+server.listen(port, () => {
+    routes.forEach((route: CommonRoutesConfig) => {
+        debugLog(`Routes configured for ${route.getName()}`);
+    });
+    // our only exception to avoiding console.log(), because we
+    // always want to know when the server is done starting up
+    console.log(runningMessage);
+});
---
+++ users/routes.config.ts
@@ -0,0 +1,37 @@
+import { CommonRoutesConfig } from '../common/routes.config';
+import { Request, Response, Application, NextFunction } from 'express';
+
+export class UsersRoutes extends CommonRoutesConfig {
+    constructor(app: Application) {
+        super(app, 'UsersRoutes');
+    }
+
+    configureRoutes() {
+        this.app.route(`/v1/users`)
+                .get((req: Request, res: Response) => {
+                    res.status(200).send(`List of users`);
+                })
+                .post((req: Request, res: Response) => {
+                    res.status(200).send(`Post to users`);
+                });
+
+        this.app.route(`/v1/users/:userId`)
+                .all((req: Request, res: Response, next: NextFunction) => {
+                    next();
+                })
+                .get((req: Request, res: Response) => {
+                    res.status(200).send(`GET requested for id ${req.params.userId}`);
+                })
+                .put((req: Request, res: Response) => {
+                    res.status(200).send(`PUT requested for id ${req.params.userId}`);
+                })
+                .patch((req: Request, res: Response) => {
+                    res.status(200).send(`PATCH requested for id ${req.params.userId}`);
+                })
+                .delete((req: Request, res: Response) => {
+                    res.status(200).send(`DELETE requested for id ${req.params.userId}`);
+                });
+
+        return this.app;
+    }
+}
